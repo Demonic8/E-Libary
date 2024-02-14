2104a5eff2f264c4c0d071272536e03c8ae3321e
acer <jaramillarodent@gmail.com>
2024-02-14 10:30:44 +0800

    New Commit

---
+++ app/src/main/java/com/example/e_libary/Verifiying.java
@@ -0,0 +1,14 @@
+package com.example.e_libary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Verifiying extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifiying);
+    }
+}
