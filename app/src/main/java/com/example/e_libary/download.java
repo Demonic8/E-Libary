6f37e0a5b7370780c8c84d71abf3d742d47bf187
acer <jaramillarodent@gmail.com>
2024-02-15 15:07:09 +0800

    collection and download page

---
+++ app/src/main/java/com/example/e_libary/download.java
@@ -0,0 +1,46 @@
+package com.example.e_libary;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.example.e_libary.R;
+import android.media.Image;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.os.Bundle;
+
+public class download extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ImageButton navButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navButton = findViewById(R.id.navigation_button);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            // The Nav View Area
+            @Override
+            public void onClick(View v) {
+
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
